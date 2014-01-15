67649352d77f39e07e0ccfed73471dd3e120099d
Beth Aitman <beth.aitman@red-gate.com>
2014-01-15 15:54:18 +0000

    fiddling with node (doesn't work)

--- tabula.js
+++ tabula.js
@@ -9,7 +9,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.static(__dirname + '/public'));
-app.engine('html', require('ejs').renderFile);
 
 app.get('/', function (req, res) {
   res.render('tabuladisplay');
