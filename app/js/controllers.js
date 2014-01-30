074440570a5f7e7416a019f4331f72b4865bfef6
Beth Aitman <beth.aitman@red-gate.com>
2014-01-30 14:03:19 +0000

    ordering, and exit status colours

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,4 +8,34 @@ tabulaApp.controller('TabulaCtrl', function ($scope, $http) {
   $http.get('example.json').then(function (res) {
     $scope.entries = res.data;
   });
+  $scope.orderProp = 'entry.startTime';
+
+  $scope.getShortDir = function (cwd) {
+    var comps = cwd.split("/");
+    return  "../" + comps[comps.length - 1];
+  };
+  
+  $scope.getDate = function(timestamp) {
+    var comps = timestamp.split("T");
+  return comps[0];
+  };
+  
+  $scope.getTime = function(timestamp) {
+    var comps = timestamp.split("T");
+  return comps[1];
+  };
+  
+  $scope.exitStatus = function(exitStatus) {
+    if (exitStatus === 0) {
+      // Successful
+      return "exitSuccess";
+    } else if (exitStatus > 127) {
+      // Killed by user
+      return "exitKilled";
+    } else {
+      // Failed for some reason.
+      return "exitFailure";
+    }
+  };
+  
 });
