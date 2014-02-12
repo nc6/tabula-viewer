7d705e2db711d5227199e463d07926a27bcb5622
Beth Aitman <beth.aitman@red-gate.com>
2014-02-12 13:49:47 +0000

    Still can't get data into observablearray

--- app/js/viewmodel.js
+++ app/js/viewmodel.js
@@ -1,17 +1,24 @@
 function ViewModel() {
   var self = this;
+  self.entries = ko.observableArray();
+  self.loadEntries = function (data) {
+    data.forEach(function(e){
+      self.entries.push(e);
+      alert("banana");
+    });
+  };
+
+  (function ($) {
+    $(document).ready(function () {
+      $('#btn-load-project').click(function () {
+        var project = $('#project-name').val();
+        $.getJSON(project, self.loadEntries);
+      });
+    });
+  })(jQuery);
 }
+
+
 
 // Activates knockout.js
 ko.applyBindings(new ViewModel());
-
-
-
-(function ($) {
-  $(document).ready(function () {
-    $('#btn-load-project').click(function () {
-     var project = $('#project-name').val();
-     $.getJSON(project, renderData);
-   });
-  });
-})(jQuery);
