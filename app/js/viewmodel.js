9e82ef01246877f0665bac8d3cceb219faab872c
Beth Aitman <beth.aitman@red-gate.com>
2014-02-12 10:54:16 +0000

    Starting to get knockout to work - load json

--- app/js/viewmodel.js
+++ app/js/viewmodel.js
@@ -1,5 +1,17 @@
-function TabulaViewModel() {
+function ViewModel() {
+  var self = this;
 }
 
 // Activates knockout.js
-ko.applyBindings(new AppViewModel());
+ko.applyBindings(new ViewModel());
+
+
+
+(function ($) {
+  $(document).ready(function () {
+    $('#btn-load-project').click(function () {
+     var project = $('#project-name').val();
+     $.getJSON(project, renderData);
+   });
+  });
+})(jQuery);
