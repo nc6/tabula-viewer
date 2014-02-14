87ccbdd94371b851e227397858a655147126463a
Beth Aitman <beth.aitman@red-gate.com>
2014-02-14 13:36:12 +0000

    Further attempts at tabs
    The logic in the controller is now fixed, but the display still doesn't
    work

--- app/js/viewmodel.js
+++ app/js/viewmodel.js
@@ -1,13 +1,33 @@
 function ViewModel() {
   var self = this;
   self.entries = ko.observableArray();
-  self.loadEntries = function (data) {
+  
+  self.loadEntries = function(data) {
     self.entries.removeAll();
     data.forEach(function(e){
       self.entries.push(e);
     });
   };
-
+  
+  self.exitStatusClass = ko.computed(function(status){
+    if (status===0)
+      return 'exitSuccess';
+    if (status>0 && status<128)
+      return 'exitFailure';
+    if (status>128)
+      return 'exitKilled';
+  });
+  
+  self.changeTab = function(tab) {
+    if (!this.hasOwnProperty('selectedTab')) {
+      this.selectedTab = tab;
+    } else if (this.selectedTab == tab) {
+      this.selectedTab = 'none';
+    } else {
+      this.selectedTab = tab;
+    }
+  };
+  
   (function ($) {
     $(document).ready(function () {
       $('#btn-load-project').click(function () {
