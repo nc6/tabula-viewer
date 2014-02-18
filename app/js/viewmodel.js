952dcdce287b7f44f96a80a93a895fd74ab7672d
Beth Aitman <beth.aitman@red-gate.com>
2014-02-18 18:32:53 +0000

    Failed attempt at popovers

--- app/js/viewmodel.js
+++ app/js/viewmodel.js
@@ -2,25 +2,28 @@ function ViewModel() {
   var self = this;
   self.entries = ko.observableArray();
   
-  self.loadEntries = function(data) {
+  self.loaded = ko.observable(false);
+  
+  self.loadEntries = function (data) {
     self.entries.removeAll();
-    data.forEach(function(e){
+    data.forEach(function (e) {
       e.entry.selectedTab = ko.observable("none");
       e.entry.dirLength = ko.observable("short");
       self.entries.push(e);
     });
+    self.loaded(true);
   };
-  
-  self.exitStatusClass = ko.computed(function(status){
-    if (status===0)
+
+  self.exitStatusClass = ko.computed(function (status) {
+    if (status === 0)
       return 'exitSuccess';
-    if (status>0 && status<128)
+    if (status > 0 && status < 128)
       return 'exitFailure';
-    if (status>128)
+    if (status > 128)
       return 'exitKilled';
   });
-  
-  self.changeTab = function(newTab) {
+
+  self.changeTab = function (newTab) {
     // 'this' is 'entry.selectedTab'
     if (this() === newTab) {
       this('none');
@@ -28,8 +31,8 @@ function ViewModel() {
       this(newTab);
     }
   };
-  
-  self.changeDirLength = function() {
+
+  self.changeDirLength = function () {
     // 'this' is 'entry.dirLength'
     if (this() === 'short') {
       this('long');
@@ -37,18 +40,18 @@ function ViewModel() {
       this('short');
     }
   };
-  
-  self.shortDir = function(dir) {
+
+  self.shortDir = function (dir) {
     var comps = dir.split("/");
     return "../" + comps[comps.length - 1];
   };
-  
-  self.getDate = function(dateTime) {
+
+  self.getDate = function (dateTime) {
     var comps = dateTime.split("T");
     return comps[1];
-    
+
   };
-  
+
   (function ($) {
     $(document).ready(function () {
       $('#btn-load-project').click(function () {
@@ -59,6 +62,24 @@ function ViewModel() {
   })(jQuery);
 }
 
+ko.bindingHandlers.popoverInit = {
+  // This will be called when the binding is first applied to an element
+  init: function (element) {
+    element = $(element);
+    var contentSelector = element.data("popover-content-selector");
+    var contentElement = $(contentSelector);
+    $(contentElement).hide();
+
+    element.popover({
+      html: true,
+      content: contentElement.html(),
+      trigger: "click",
+      title: element.attr("title"),
+      placement: "bottom",
+      container: '.large-bootstrap-popover'
+    });
+  }
+};
 
 
 // Activates knockout.js
